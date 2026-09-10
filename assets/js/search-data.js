2dde38486e6a80efca0e9f06ab09ed6d426b82ca
tulga-rdn <128572046+tulga-rdn@users.noreply.github.com>
2026-09-10 17:28:14 +0000

    Deploying to gh-pages from @ tulga-rdn/tulga-rdn.github.io@b1a9cb749e5a0b00c149180ee397622f462ee2ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-sars-cov-2-genome-analysis",
+        },{id: "post-analyzing-workplace-discrimination",
+        
+          title: "Analyzing workplace discrimination",
+        
+        description: "analyzing workplace discrimination using data",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/mas456/";
+          
+        },
+      },{id: "post-sars-cov-2-genome-analysis",
         
           title: "SARS-CoV-2 genome analysis",
         
@@ -59,51 +70,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_4/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+            },},{id: "news-lorem-is-out-in-tmlr",
+          title: 'LOREM is out in TMLR!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_5/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
